0bd400d225c08f865c0cd27b4076da1fbe44f23a
seveneleves <paullevent2014@u.northwestern.edu>
2019-08-17 16:23:34 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+  document.forms['user_dropdown'].reset();
+}
+
+let addresses = ['ABO', '0x79392aC67F8531C38f4779d586dCa05F31c9d330', 'FMI', '0x32f5f250b5a1ce58da9edd6d418369c510e6fa2c', 'PLE', '0xebB0B73575D8113D4Bd6D6490eeE97e5FA780EBe', 'TAS', '0x74863C843305F85B52F86f8BD2729D2e2c613B7e', 'VIL', '0x668bfF5Cb132f6c9c078f13c6c5C01d409abEDa9'];
+
+let nameList = document.getElementById('name');
+let qr = document.getElementById('qr_code');
+let address = document.getElementById('ether_address');
+let copyButton = document.getElementById('copy_button');
+let copyPopup = document.getElementById('popup')
+let closeButton = document.getElementById('close_img');
+
+nameList.addEventListener('change', function(e) {
+  if (e.target.value == 'choose') {
+    qr.style.display = 'none';
+    document.querySelector('.ether_address_area').style.display = 'none';
+  } else {
+      qr.style.display = 'block';
+      qr.src = 'qr_codes/' + e.target.value + '.png';
+      document.querySelector('.ether_address_area').style.display = 'flex';
+      address.textContent = addresses[addresses.indexOf(e.target.value)+1];
+  }
+})
+
+copyButton.addEventListener('click', function() {
+  copyPopup.style.display = "flex";
+})
+
+closeButton.addEventListener('click', function() {
+  copyPopup.style.display = "none";
+})
